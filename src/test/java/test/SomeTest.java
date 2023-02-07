6a85cc6d4acd59803712bcf46fb4be4fb80d3d0c
pruthvi1902 <119110120+pruthvi1902@users.noreply.github.com>
2023-02-07 18:38:32 +0530

    Update SomeTest.java

--- src/test/java/test/SomeTest.java
+++ src/test/java/test/SomeTest.java
@@ -26,6 +26,25 @@ package test;
 
 import org.junit.Test;
 
+@Test public void test1() {
+        run();
+    }
+
+    @Test public void test2() {
+        run();
+    }
+
+    @Test public void test3() {
+        run();
+    }
+
+    @Test public void test4() {
+        run();
+    }
+
+    @Test public void test5() {
+        run();
+    }
 public class SomeTest extends Base {
     @Test public void test6() {
         run();
